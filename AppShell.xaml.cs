f5bfd64fb263aba2dca8ceb118bbf26314f50882
Suresh Ghimire <sghimire38288@ucumberlands.edu>
2026-02-10 10:28:09 -0600

    inital commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace tracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MapPage.cs
@@ -0,0 +1,13 @@
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
+namespace tracker
+{
+    public class MapTypesPageCode : ContentPage
+    {
+        public MapTypesPageCode()
+        {
+            Map map = new Map();
+            Content = map;
+        }
+    }
+}
